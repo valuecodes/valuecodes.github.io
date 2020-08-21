7c35044676afd39c5bb225f711f4dcce779e71c1
Tietohallinto <tietohallinto@BC-minimac053.local>
2020-08-21 11:18:04 +0300

    Dom exercises 2 2

--- js/scripts.js
+++ js/scripts.js
@@ -12,13 +12,12 @@ document.getElementsByTagName("body")[0].style.fontFamily = "Impact";
 
 let items = document.getElementsByClassName("item");
 
-console.log(items);
 for (var i = 0; i < items.length; i++) {
   items[i].style.margin = "10px 20px";
   items[i].style.boxShadow = "2px 2px 10px";
 }
 
-let navbar = document.getElementsByClassName("navbar-nav mr-auto")[0];
+let navbar = document.getElementsByClassName("navbar-nav")[0];
 
 navbar.appendChild(
   '<li class="nav-item menu-large"><a class="nav-link">Newsletter</a></li>'
