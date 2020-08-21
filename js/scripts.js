fa5ecf0b66e6c415b1d6f835cbc80498c6e66ad2
Tietohallinto <tietohallinto@BC-minimac053.local>
2020-08-21 10:43:28 +0300

    Dom exercises font family

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,5 @@ console.log("Hello world");
 document.getElementById("slogan").textContent = "We Hate Our Customers";
 
 document.getElementById("all").style.backgroundColor = "#c7c3c3";
+
+document.getElementsByTagName("body").style.fontFamily = "Impact";
