406ec805f1c654bc0eaa096d1cbcaa85ef381ae5
Tietohallinto <tietohallinto@BC-minimac053.local>
2020-08-21 10:57:32 +0300

    Dom exercises 2 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Level 1
+
 console.log("Hello world");
 
 document.getElementById("slogan").textContent = "We Hate Our Customers";
@@ -5,3 +7,8 @@ document.getElementById("slogan").textContent = "We Hate Our Customers";
 document.getElementById("all").style.backgroundColor = "#c7c3c3";
 
 document.getElementsByTagName("body")[0].style.fontFamily = "Impact";
+
+// Level 2
+
+document.getElementsByClassName("item").style.margin = "10px 20px";
+document.getElementsByClassName("item").style.boxShadow = "2px 2px 10px";
