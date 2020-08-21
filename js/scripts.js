5c4d0720f4f2be14b047045ba4415b9898e91923
Tietohallinto <tietohallinto@BC-minimac053.local>
2020-08-21 11:03:04 +0300

    Dom exercises 2 1

--- js/scripts.js
+++ js/scripts.js
@@ -10,5 +10,9 @@ document.getElementsByTagName("body")[0].style.fontFamily = "Impact";
 
 // Level 2
 
-document.getElementsByClassName("item").style.margin = "10px 20px";
-document.getElementsByClassName("item").style.boxShadow = "2px 2px 10px";
+let items = document.getElementsByClassName("item");
+
+items.forEach((item) => {
+  item.style.margin = "10px 20px";
+  item.style.boxShadow = "2px 2px 10px";
+});
